e82cef7582e91c69ea6cad4bc6ad4ab98878791d
Claudia Petty <claudialove32@gmail.com>
2020-04-06 19:16:28 -0400

    debugging but also getting rid of the extra directory

--- password-generator.js
+++ password-generator.js
@@ -1,6 +1,5 @@
 
-//start
-window.setTimeout(function () {
+
 
     //user PROMPT for desired length (come in from prompt as a string)
     var passwordLength = prompt("PASSWORD LENGTH: pick a number from 8-128 for required password length: ");
@@ -33,17 +32,43 @@ window.setTimeout(function () {
         //EMPTY STRING
         var randomPassword = "";
         //FOR LOOP on required length of password
-        for (var i = 0; i < requiredLength.length; i++) {
+        for (var i = 0; i < requiredLength; i++) {
             //picks a character within charSet at index of random number
             randomPassword += useChar.charAt(Math.floor(Math.random() * useChar.length));
         }
         return randomPassword;
-        console.log(randomPassword);
     }
     alert(generateRandomPassword());
 
-}, 500);
 
 
 // generateBtn.addEventListener("click", writePassword);   
 
+/*var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+
+//ask for character type
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+//generate password
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+alert(generatePassword());*/
